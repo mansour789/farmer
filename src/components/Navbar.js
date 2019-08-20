83d36bd37c4917febccaba11cb3fe2623fe647f5
nadaalotaibii <naaadaa_tb@yahoo.com>
2019-08-20 12:18:27 +0300

    update readme

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,13 +15,12 @@ class Navbar extends Component{
       <div class="col s12"> 
         <a href="" class="brand-logo"> <img class="material-icons" style={{"height":"62px", "border-radius":"20%"}} src="./img/logo.png"/></a>
        
-        <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger black-text"><i class="material-icons">menu</i></a>
           <ul id="nav-mobile" class="right hide-on-med-and-down">
 
    
             <li> <Link to="/" className="black-text">Home</Link> </li>
             <li> <Link to="/aboutus" className="black-text">About</Link> </li>
-           
             
           </ul>
       </div> 
