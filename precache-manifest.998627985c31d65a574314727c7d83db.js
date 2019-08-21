7d0ca42919f94b192be818f40ed139e3ffa320fd
mansour789 <m0509111136@gmail.com>
2019-08-21 16:05:17 +0300

    Updates

---
+++ precache-manifest.998627985c31d65a574314727c7d83db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be71489dff30cfec2ccc674f62ae611b",
+    "url": "/farmer/index.html"
+  },
+  {
+    "revision": "02f7757ae6b68cd72767",
+    "url": "/farmer/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "e7e04f9e10895374cdba",
+    "url": "/farmer/static/css/main.f009983f.chunk.css"
+  },
+  {
+    "revision": "02f7757ae6b68cd72767",
+    "url": "/farmer/static/js/2.80d21cda.chunk.js"
+  },
+  {
+    "revision": "e7e04f9e10895374cdba",
+    "url": "/farmer/static/js/main.a05b5379.chunk.js"
+  },
+  {
+    "revision": "f717622e7241a61ee1d2",
+    "url": "/farmer/static/js/runtime~main.c44a7206.js"
+  }
+]);
