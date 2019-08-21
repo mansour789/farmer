ef7b75af4969bb84294a435fac517006e2332d47
mansour789 <m0509111136@gmail.com>
2019-08-21 15:01:42 +0300

    kk

--- src/Context.js
+++ src/Context.js
@@ -19,7 +19,7 @@ class PlantProvidor extends Component {
       loading: true
     });
     var body = {
-      'key': PLANT_API,
+      // 'key': PLANT_API,
       'usage_info': true,
       'images': [srcData]
     };
@@ -46,7 +46,7 @@ class PlantProvidor extends Component {
               Accept: "application/json"
             },
             body: JSON.stringify({
-              'key': PLANT_API,
+              // 'key': PLANT_API,
               'ids': [parseInt(data.id)]
             })
           })
@@ -57,7 +57,8 @@ class PlantProvidor extends Component {
               console.log(" image", data[0].images[0].url);
               this.setState({
                 data: data[0].suggestions[0],
-                image: data[0].images[0].url
+                image: data[0].images[0].url,
+                loading: false
               });
               //Please Active This
               // this.moreInfo(suggestions[0].plant.name);
